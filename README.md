2b631c3723ba5e3b4b5b27fee69d19d8ab3df891
itsDru157 <carlosmateus7720@gmail.com>
2024-04-13 21:25:14 -0300

    d

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-Hello guys, this scrips works like a password generetor with a length of the 12 caracter, including sybobolos. I styied some packges like string, and I knew a little bit of random cause i have worked with.
+Hello guys, this scrips works like a password generetor with a length of the 12 character, including sybobolos. I styied some packges like string, and I knew a little bit of random cause I have worked with.
+
+13/04
+create a new function that can create a file and write the log's of the password generator.
+
+
+
+
 
--- projeto/main.py
+++ projeto/main.py
@@ -8,15 +8,32 @@ tamanho_senha = 0
 letras_minusculas = s.ascii_lowercase
 numeros = "0123456789"
 simbolos = "!@#$%^&*()-_+={}[]|;':<>,.?/"
+def gerar_historico():
+    f = True
+    if f == True:
+        gerar_senha(12)# Chamando a função com o tamanho da senha desejado
+    else:
+        f = open("historioco de senha (dev).txt", "x")
+        print("arquivo de log criado, reexecute o codigo para criar a senha.")
+        exit
+
 def gerar_senha(tamanho_senha):
     todas_letras = list(letras_minusculas + numeros + simbolos)  # Converte em lista
     r.shuffle(todas_letras)
     senha = "".join(map(str, r.sample(todas_letras, tamanho_senha)))
+    f= open("historioco de senha (dev).txt","a")
+    f.write(senha)
+    f.close()
     print(senha)
+    
+    
+    
+print("sua senha ficara disponivel por 60 segundos: ")
 print("Sua senha é: ")
 
-gerar_senha(12)  # Chamando a função com o tamanho da senha desejado
-time.sleep(12)
+
+gerar_historico()
+
     
         
         
